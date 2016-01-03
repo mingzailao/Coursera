5cf1abe615cdd8fcacc3138df97b4a38b4b214db
mingzailao <mingzailao@163.com>
2016-01-03 16:35:27 +0800

    solve Q3 of week4

--- C_up/week4/test3.cc
+++ C_up/week4/test3.cc
@@ -1,5 +1,37 @@
 #include<iostream>
+#include<stdio.h>
+#include<string.h>
 using namespace std;
-int main(){
-
+int main()
+{
+    int n,i;
+    char a[45]={'\0'},b[85]={'\0'};
+    int lenA=0,lenB=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%s",a);
+        lenA=strlen(a);
+        if(lenB+1+lenA<79)
+        {
+            if(lenB>0)
+                strcat(b," ");
+            strcat(b,a);
+            lenB=strlen(b);
+        }
+        else if(lenB+1+lenA==80||lenB+1+lenA==79)
+        {
+            printf("%s %s\n",b,a);
+            lenB=0;
+            b[0]='\0';
+        }
+        else
+        {
+            printf("%s\n",b);
+            strcpy(b,a);
+            lenB=strlen(b);
+        }
+    }
+    printf("%s\n",b);
+    return 0;
 }
